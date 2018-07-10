cbb6a4dda47672e5bb966bf9b9a2751e52db7952
franckmartin3d <franck.martin3d@gmail.com>
2018-07-10 23:29:52 +1000

    Fix printing

---
+++ MLB_lbc.py
@@ -0,0 +1,17 @@
+# Lunch Box creator
+
+
+# -----------------------------------------> CLASS <--------------------------------------------------------------------
+
+
+#Item class
+
+class Item(object):
+    """Creating items object to use by the lunchbox"""
+
+#Attribute of the class , Title, Nutitious value, type
+
+    def __init__(self, title, nvalue,type):
+
+
+
