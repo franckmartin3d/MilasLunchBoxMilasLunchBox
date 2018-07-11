be307b663d08e369e913cdb7d4bced2fcd9ee504
francoism <franck.martin3d@gmail.com>
2018-07-11 19:29:21 +1000

    class lunchitem

--- MLB_lbc.py
+++ MLB_lbc.py
@@ -6,12 +6,29 @@
 
 #Item class
 
-class Item(object):
+
+
+
+class LunchItem(object):
     """Creating items object to use by the lunchbox"""
+    item = 0
+
+# list of instance objects
+    itemlist = []
+
 
 #Attribute of the class , Title, Nutitious value, type
 
-    def __init__(self, title, nvalue,type):
+    def __init__(self, title, nvalue, type):
+        self.title = title
+        self.nvalue = float(nvalue)
+        self.type = type
+        LunchItem.total +=1
 
+    # Add to itemlist
+        LunchItem.itemlist.append(self)
 
-
+# Display the object when using print
+    def __str__(self):
+        rep = str.upper(self):
+        rep =
