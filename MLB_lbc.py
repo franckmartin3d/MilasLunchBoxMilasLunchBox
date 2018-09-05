404378dfd6224d7ead2dfc327854ce538006b3b0
franckmartin3d <franck.martin3d@gmail.com>
2018-09-06 07:15:22 +1000

    Added LunchItem function and Main Menu

--- MLB_lbc.py
+++ MLB_lbc.py
@@ -18,9 +18,23 @@ class LunchItem(object):
     vegylist = []
     fruitlist = []
     dairylist =[]
+    dessertlist = []
 
+# Create an instance user input
+    @staticmethod
+    def userinstance():
+        print("Creating Lunch Item  instance")
+        lunchitem_instance = input("What is the Lunch Item title?:\n")
 
+        lunchitem_instance = LunchItem(lunchitem_instance, input("Enter your nutriment value: \n"),
+                                 input("Enter your recipe instruction: \n"), input("Enter your lunch Item Type: \n"))
+        print("--Lunch Item--")
+        return lunchitem_instance
 
+# Update how many there is
+    @staticmethod
+    def status():
+        print("\nThe total number of Lunch item is ", LunchItem.item)
 
 #Attribute of the class , Title, Nutitious value, type
 
@@ -36,11 +50,17 @@ class LunchItem(object):
         if self.type == "meal" or self.type == "Meal":
             LunchItem.meallist.append(self)
 
-        if self.type == "vegy" or self.type == "Vegy":
-            LunchItem.vegylist.append(self)
+        elif self.type == "vegy" or self.type == "Vegy":
+                LunchItem.vegylist.append(self)
 
-        if self.type == "fruit" or self.type == "frui":
-            LunchItem.fruitlist.append(self)
+        elif self.type == "fruit" or self.type == "Fruit":
+                LunchItem.fruitlist.append(self)
+
+        elif self.type == "desert" or self.type == "Desert":
+                LunchItem.dessertlist.append()
+
+        else: self.type == "desert" or self.type == "Desert"
+                LunchItem.dessertlist.append()
 
 # Display the object when using print
     def __str__(self):
@@ -73,9 +93,93 @@ def vegylist():
     for i, val in enumerate(LunchItem.vegylist):
         print("\t", i, val.title)
 
-def itemlist():
+def fruitlist():
     """Display all item """
 
     print("\t---Item Available---")
-    for i, val in enumerate(LunchItem.itemlist):
+    for i, val in enumerate(LunchItem.fruitlist):
         print("\t", i, val.title)
+
+def dairylist():
+    """Display all item """
+
+    print("\t---Item Available---")
+    for i, val in enumerate(LunchItem.dairylist):
+        print("\t", i, val.title)
+
+def desertlist():
+    """Display all item """
+
+    print("\t---Item Available---")
+    for i, val in enumerate(LunchItem.desertlist):
+        print("\t", i, val.title)
+
+
+# -------------------------------------------> Helping Functions <----------------------------------------------------
+
+
+# press enter to continue
+def pressEnter():
+    """Press enter to continue"""
+    try:
+        input("\nPress enter to continue")
+        #   print("\n" * 15)
+    except SyntaxError:
+        pass
+
+# clear IDLE
+def clear():
+    print("\n" * 15)
+    print(
+        "===========================================================================================================")
+
+
+# -------------------------------------------------> MAIN MENU <--------------------------------------------------------
+
+#main menu can i put that into its own class?
+def menuPrincipal():
+    """Main Menue"""
+    selection = None
+
+    while selection != "0":
+        print("""
+        Lunch BOX Creator 
+        
+        MAIN MENU
+        
+        LB
+        1 - List All Available lunchbox
+        2 - Create a Manual Lunchbox
+        3 - Generate a Lunchbox
+        
+        4 - Go to Lunchbox Items Menue
+        
+        0 - EXIT
+        
+        """)
+
+    selection = input("Selection: \n")
+
+    if selection == "1":
+        # TO DO : lunchbox object
+        print("Lunch Box Available:")
+
+        # TO DO : Lunch box object & generating auto lunchbox
+    elif selection == "2":
+        print ("Creating Manual Lunchbox")
+
+        # TO DO : generating auto lunchbox
+    elif selection == "3":
+        print ("Generating Lunchbox")
+
+        #TO DO : Lunch Item menu
+    elif selection == "4":
+        print ("Welcome to lunch item Menu")
+
+    elif selection == "0":
+        print("Quiting Recipe Main...")
+
+    else:
+        print("Input invalid choose 1-4 or 0")
+
+
