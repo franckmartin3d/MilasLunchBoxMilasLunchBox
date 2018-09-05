d1199c09a5e9529c615fe98b1f6690083f93df4d
francoism <franck.martin3d@gmail.com>
2018-09-05 18:12:02 +1000

    class lunchitem part 2

--- MLB_lbc.py
+++ MLB_lbc.py
@@ -4,11 +4,7 @@
 # -----------------------------------------> CLASS <--------------------------------------------------------------------
 
 
-#Item class
-
-
-
-
+#Item class , items that can be added to the slots of a lunchbox
 class LunchItem(object):
     """Creating items object to use by the lunchbox"""
     item = 0
@@ -16,20 +12,52 @@ class LunchItem(object):
 # list of instance objects
     itemlist = []
 
+# list by type
+
+    meallist = []
+    vegylist = []
+    fruilist = []
+    dairylist =[]
+
+
+
 
 #Attribute of the class , Title, Nutitious value, type
 
     def __init__(self, title, nvalue, type):
         self.title = title
-        self.nvalue = float(nvalue)
+        self.nvalue = int(nvalue)
         self.type = type
         LunchItem.total +=1
 
     # Add to itemlist
         LunchItem.itemlist.append(self)
 
+        if self.type == "mael" or self == "Meal":
+            LunchItem.meallist.append(self)
+
+
+
 # Display the object when using print
     def __str__(self):
         rep = str.upper(self.title) + '\n\n'
         rep += str.rep += "Nutitious Value: \n" + self.nvalue + " \n\n"
         rep += "type: \n" + self.type + " \n\n"
+
+#Display the object method
+    def display(self):
+        print("Title: \n\n" +self.title, "\n")
+        print("value: \n\n" + self.nvalue + " \n")
+        print("type: \n\n" + self.type + " \n")
+
+
+
+# -----------------------------------------> functions <--------------------------------------------
+
+# show what item there is
+def itemlist():
+    """Display all item """
+
+    print("\t---Item Available---")
+    for i, val in enumerate(Recipe.rlist):
+        print("\t", i, val.title)
